e7b5411b8d9a56b2708c2d089b2efccc3299a334
Tutoz <tutos_aaf@hotmail.com>
2024-03-29 05:54:01 -0300

    first commit

---
+++ personajes.js
@@ -0,0 +1,37 @@
+const URL = 'https://valorant-api.com/v1/agents';
+const parametro1 = 'valor1';
+const parametro2 = 'valor2';
+
+let agentesLista = [];
+
+// Construir la URL de la solicitud con los parámetros
+const API = `${URL}?language=es-MX&isPlayableCharacter=True`;
+fetch(API)
+.then(response => {
+  return response.json();
+})
+.then(data => {
+
+  const DATA = data.data;
+  let lista = [{
+    nombre: DATA.map(item => item.displayName),
+    rol: DATA.map(item => item.role.displayName),
+    img: DATA.map(item => item.displayIcon)
+  }]
+
+  lista = lista[0];
+  
+  for (let i = 0; i < lista.nombre.length; i++) {
+    if (lista.nombre[i] == "Sage" || lista.nombre[i] == "Skye" ){
+      lista.rol[i] = "Sanador"
+    }
+    agentesLista[i] = {
+      nombre: lista.nombre[i],
+      rol: lista.rol[i],
+      img: lista.img[i]
+    }
+  }
+
+
+  console.log(agentesLista);
+});
---
+++ script.js
@@ -0,0 +1,131 @@
+// objetos
+const btnGenerar = document.getElementById("btnGenerar");
+
+// eventos
+btnGenerar.addEventListener("click", Random);
+load();
+
+// variables
+let oldOutput;
+
+// funciones
+function Random() {
+    let agentesActuales = [];
+    let rolesActuales = [];
+    let jugadores = traerJugadores();
+
+    for (let i = 0, len = jugadores.length; i < len; i++) {
+        const rol = elegirRolRandom(rolesActuales);
+        rolesActuales.push(rol);
+
+        const agente = elegirPersonajeRandom(agentesActuales, rol);
+        agentesActuales.push(agente);
+    }
+    save();
+    crearTarjeta(agentesActuales);
+}
+function crearTarjeta(Agentes){
+    const main = document.getElementById("main");
+    const aside = document.createElement("aside");
+    if (oldOutput != undefined){
+        oldOutput.outerHTML = "";
+    }
+    aside.id = "aside";
+    const div1 = document.createElement("div");
+    div1.classList.add("player-output-div");
+
+    let jugadores = traerJugadores();
+    let i = 0;
+    Agentes.forEach(agente => {
+        const articulo = document.createElement("article");
+        articulo.classList.add("player-article");
+
+        const jugador = document.createElement("label");
+        jugador.textContent = jugadores[i];
+        console.log(jugadores);
+        jugador.classList.add("player-output-name");
+        i++;
+
+        const br = document.createElement("br");
+
+        const div2 = document.createElement("div");
+        div2.classList.add("character-output-data");
+
+        const imagen = document.createElement("img");
+        imagen.src = agente.img;
+        imagen.classList.add("player-output-img");
+        
+        const div3 = document.createElement("div");
+        div3.classList.add("character-output-data");
+
+        const personaje = document.createElement("label");
+        personaje.textContent = agente.nombre;
+        personaje.classList.add("character-name");
+
+        const rol = document.createElement("label");
+        rol.textContent = agente.rol;
+        rol.classList.add("character-rol");
+
+        aside.appendChild(div1);
+        div1.appendChild(articulo);
+        articulo.appendChild(jugador);
+        articulo.appendChild(br);
+        articulo.appendChild(div2)
+        div2.appendChild(imagen);
+        div2.appendChild(div3);
+        div3.appendChild(personaje);
+        div3.appendChild(rol);
+
+        main.appendChild(aside);
+      });
+      if (jugadores.length > 0){
+        oldOutput = aside;
+      }
+      else{
+        oldOutput = undefined;
+      }
+}
+function elegirPersonajeRandom(agentesActuales, rol) {
+    let agentes = agentesLista
+    agentes = agentes.filter(p => !agentesActuales.includes(p));
+    agentes = agentes.filter(p => p.rol === rol);
+
+    const num = Math.floor(Math.random() * agentes.length);
+    return agentes[num];
+}
+function elegirRolRandom(rolesActuales){
+    let roles = agentesLista.map(p => p.rol);
+    roles = roles.filter(p => !rolesActuales.includes(p));
+    const num = Math.floor(Math.random() * roles.length);
+    return roles[num];
+}
+
+function traerJugadores(){
+    let jugadores = []
+    for (let i = 1; i <= 5; i++) {
+        const p = document.getElementById("player" + i + "-input");
+        if (p.value.trim() != ""){
+            jugadores.push(p.value);
+        }
+    }
+    return jugadores;
+}
+
+function load(){
+    for (let i = 1; i <= 5; i++) {
+        const nombre = localStorage.getItem("Nombre" + i);
+        if (!nombre){
+            localStorage.setItem("Nombre" + i, '');
+        }
+        else{
+            const p = document.getElementById("player" + i + "-input");
+            p.value = localStorage.getItem("Nombre" + i);
+        }
+    }
+}
+function save(){
+    for (let i = 1; i <= 5; i++) {
+        const p = document.getElementById("player" + i + "-input");
+        localStorage.setItem("Nombre" + i, p.value);
+    }
+}
